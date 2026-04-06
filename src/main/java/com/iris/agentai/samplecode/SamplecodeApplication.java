8f26cafec849e3102b08040786c9dc1b369edf97
Mohit Agarwal <mohit.agarwal@irissoftware.com>
2026-04-06 15:28:17 +0530

    adding exception

--- src/main/java/com/iris/agentai/samplecode/SamplecodeApplication.java
+++ src/main/java/com/iris/agentai/samplecode/SamplecodeApplication.java
@@ -1,12 +1,22 @@
 package com.iris.agentai.samplecode;
 
+import org.apache.logging.log4j.spi.LoggerContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SamplecodeApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SamplecodeApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("In main method");
+		int a= 0;
+		int b= 10;
+		int cal = b/a;
+
 		SpringApplication.run(SamplecodeApplication.class, args);
 	}
 
