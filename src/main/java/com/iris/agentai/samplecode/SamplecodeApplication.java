7398d6bedce48b2cb0972cb36a8c589b0977709e
Mohit Agarwal <mohit.agarwal@irissoftware.com>
2026-04-06 15:18:09 +0530

    sample commit

--- src/main/java/com/iris/agentai/samplecode/SamplecodeApplication.java
+++ src/main/java/com/iris/agentai/samplecode/SamplecodeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SamplecodeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("mohit");
 		SpringApplication.run(SamplecodeApplication.class, args);
 	}
 
